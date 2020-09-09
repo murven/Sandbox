ae99ba72f96a0692d9449f90b194121a39d60cc4
Andres Romero <andresromero472@gmail.com>
2020-09-09 14:24:35 -0600

    Evaluation/Andres

---
+++ andres/web/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <project />
+  </div>
+</template>
+<script>
+import Project from '@/components/Project.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    Project
+  }
+}
+</script>
