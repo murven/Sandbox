6c39aaa3f793dc6d1dbfd54e8ae8f9660acd95f1
Andres Romero <andresromero472@gmail.com>
2020-09-13 22:55:01 -0600

    Cambios en API Routing

---
+++ a/test/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    '<div id="app">{{ message }}</div>'
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios'
+
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: 'NULL'
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:37851/')
+      .then(response => (this.info = response))
+  }
+})
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
