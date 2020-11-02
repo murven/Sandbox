d34be3b20ecb22650bbd4c242846b2ee2cf7f653
andevcr <andrescr94@outlook.com>
2020-11-02 16:57:50 -0600

    videoAsk prueba 1

---
+++ andrescr/videoAsk/app.ts
@@ -0,0 +1,11 @@
+let message: string = "Probando videoAsk!";
+
+// create a new heading 1 element
+let heading = document.createElement("h1");
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
+
+console.log('started server...');
+
+
