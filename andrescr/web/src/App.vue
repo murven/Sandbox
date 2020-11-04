df038a2b2271bd9d4430e63246f25822a024b7a1
andevcr <andrescr94@outlook.com>
2020-11-04 00:22:58 -0600

    old changes

--- andrescr/web/src/App.vue
+++ andrescr/web/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld v-bind:baseURL="baseURL"/>
+    <VideoAsk1/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import VideoAsk1 from './components/VideoAsk1.vue';
 
 @Component({
   components: {
     HelloWorld,
+    VideoAsk1
   },
 })
 export default class App extends Vue {
