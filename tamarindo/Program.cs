fded7209b400bb53801cace2790b3b18b886d8a9
Marvin Varela <marvin.varela@outlook.com>
2023-03-21 14:48:55 -0600

    adding tamarindo

---
+++ tamarindo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamarindo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = "{\"name\":\"Some name\",\"start_date\":\"2023-01-01\",\"isActive\":true}";
+        }
+    }
+}
