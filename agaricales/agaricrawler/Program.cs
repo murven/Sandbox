0264d7115ecedc9f0771db24bff8a41fa255f53a
Marvin Varela <marvin.varela@outlook.com>
2023-04-10 10:07:20 -0600

    agaricales: adding hosting capabilites

--- agaricales/agaricrawler/Program.cs
+++ agaricales/agaricrawler/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
 Console.WriteLine("Hello, World!");
+
+await host.RunAsync();
