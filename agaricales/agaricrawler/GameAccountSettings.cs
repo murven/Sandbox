9b59ee755056dbd09742a8ca3fd1d155883ff9b4
Marvin Varela <marvin.varela@outlook.com>
2023-04-10 10:54:36 -0600

    agaricales: adding game accounts configuration

---
+++ agaricales/agaricrawler/GameAccountSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace agaricrawler;
+
+internal class GameAccountSettings
+{
+    public string? Name { get; set; }
+    public string? ApiKey { get; set; }
+    public string? OutputFolder { get; set; }
+}
+
+internal class GameAccountsConfiguration
+{
+    public static string SectionName = nameof(GameAccountsConfiguration);
+    public GameAccountSettings[]? Accounts { get; set; }
+}
--- agaricales/agaricrawler/Program.cs
+++ agaricales/agaricrawler/Program.cs
@@ -1,7 +1,28 @@
-﻿using Microsoft.Extensions.Hosting;
-using System;
-using IHost host = Host.CreateDefaultBuilder(args).Build();
-
-Console.WriteLine("Hello, World!");
-
-await host.RunAsync();
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using agaricrawler;
+using System.Linq;
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+GameAccountsConfiguration? gameAccountsConfiguration =
+    config.GetRequiredSection(GameAccountsConfiguration.SectionName)
+          .Get<GameAccountsConfiguration>();
+if (gameAccountsConfiguration?.Accounts is null 
+    || !gameAccountsConfiguration.Accounts.Any())
+{
+    Console.WriteLine("No game accounts were found in the configuration.");
+    Console.WriteLine("Make sure there is an appsettings.json file in the same folder as agaricrawler.");
+    Console.WriteLine("This file should contain a section called GameAccountsConfiguration,");
+    Console.WriteLine("which in turn should include a JSON array called Accounts.");
+    Console.WriteLine("Each account should have a Name, an ApiKey and an OutputFolder to save the files to.");
+}
+else
+{
+    Console.WriteLine("The following accounts have been found in the configuration:");
+    var counter = 1;
+    foreach (var account in gameAccountsConfiguration.Accounts)
+    {
+        Console.WriteLine($"{counter:000}-) {account.Name}");
+    }
+}
